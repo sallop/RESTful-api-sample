a1c8bca19af4102648ccd81d90548b346e48baa6
sallop <shiogai1987@yahoo.co.jp>
2015-11-20 14:06:45 +0900

    add function

--- public_html/index.php
+++ public_html/index.php
@@ -5,15 +5,42 @@ require '../vendor/autoload.php';
 //echo "Hello World"
 $app = new \Slim\Slim();
 
-$app->get('/', function(){
+$app->get('/', function() use($app) {
 	echo "Home Page";
+	$app->render('index.php');
 	//$app->render('home.php');
 });
 
-$app->get('/testPage', function() use ($app){
+$app->get('/testpage', function() use ($app){
 	//echo "Test Page";
 	$app->render('testpage.php');
 });
+
+$app->get('/add_used', function() use ($app){
+	echo "add_used.php";
+	$app->render('add_used.php');
+});
+
+$app->get('/delete_used', function() use ($app){
+	echo "delete_used.php";
+	//$app->render('delete_used.php');
+});
+
+$app->get('/show_ticket', function() use ($app){
+	echo "show_ticket.php";
+	$app->render('show_ticket.php');
+});
+
+$app->get('/show_state', function() use ($app){
+	echo "show_state.php";
+	$app->render('show_state.php');
+});
+
+
+$app->get('/login', function() use ($app){
+	echo "Login Page";
+});
+
 
 $app->get('/hello/:name', function($name){
 	echo "Hello, $name";
---
+++ public_html/templates/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Slime sample</title>
+<meta charset="UTF-8">
+</head>
+<body>
+	<h1>slim sample</h1>
+	<p>please input your name</p>
+	<form action="/hello/" method="POST">
+		<input type="text" name="name" value="">
+		<input type="submit" name="submit" value="submit">
+	</form>
+</body>
+</html>
