8ac8fbd4a882efa4e5f72a2031c7ddec3402b902
sallop <shiogai1987@yahoo.co.jp>
2015-11-20 15:57:19 +0900

    add form buttons

--- public_html/add_used.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=bazaar;charset=utf8', 'tycc', '');
-//$affected_rows = $db->exec("UPDATE table SET field='value'");
-$affected_rows = $db->exec("INSERT INTO used_ticket (time) VALUES (NOW());");
-echo $affected_rows . ' were affected';
-?>
--- public_html/templates/index.php
+++ public_html/templates/index.php
@@ -3,13 +3,18 @@
 <head>
 <title>Slime sample</title>
 <meta charset="UTF-8">
+<title>food ticket counter</title>
 </head>
 <body>
 	<h1>slim sample</h1>
-	<p>please input your name</p>
-	<form action="/hello/" method="POST">
-		<input type="text" name="name" value="">
-		<input type="submit" name="submit" value="submit">
+	<form action="/add_used" method="GET">
+		<button>OK</button>
+	</form>
+	<form action="/delete_used" method="GET">
+		<button>CANCEL</button>
+	</form>
+	<form action="/show_ticket" method="GET">
+		<button>SHOW</button>
 	</form>
 </body>
 </html>
